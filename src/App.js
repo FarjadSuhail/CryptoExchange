ead708ede3f68266058ce4e121171a3c7d48b994
Farjad Suhail <farjad.suhail@systemsltd.com>
2023-06-11 02:17:11 +0500

    msg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
-// import MyApp from './classComponent/myApp.jsx';
-//import MyApp from './functionalComponent/MyApp.jsx';
-import MyApp from './Components/MyApp';
-// import MyApp2 from './Components/MyApp2';
-import Footer from './Components/Footer';
-import Login from './Components/Login';
-import RouterFile from './Components/RouterFile';
+import APICalling from './Components/APICalling';
+import Header from './Components/Header';
+import Dashboard from './Components/Dashboard';
 import { Route, Routes } from 'react-router-dom';
+import TransferCoins from './Components/TransferCoins';
 
 function App() {
+  
   return (
     <div className="App">
-      {/* <MyApp /> */}
-      {/* <MyApp2 /> */}
-      {/* <RouterFile /> */}
-      <Routes>
-        <Route path="/" element={<MyApp />} />
-        <Route path="/about" element={<Footer />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Login />} />
+       {/* <APICalling />  */}
+       {/* <Dashboard />  */}
+       <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/about" element={<APICalling />} />
+        <Route path="/transfer" element={<TransferCoins />} />
+        <Route path="*" element={<Dashboard />} />
       </Routes>
     </div>
   );
